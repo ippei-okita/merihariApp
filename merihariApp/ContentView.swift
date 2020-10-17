71168217057f530e3d567b00df625d6e1ba59ac4
ippei-okita <okpei1985@gmail.com>
2020-10-18 00:22:36 +0900

    Initial Commit

---
+++ merihariApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  merihariApp
+//
+//  Created by 大北一平 on 2020/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ merihariApp/merihariAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  merihariAppApp.swift
+//  merihariApp
+//
+//  Created by 大北一平 on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct merihariAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
